dac54c3bc5e31177ba668bb94dd2debbcb16477c
oalejel <alejel.omar@gmail.com>
2017-11-11 22:13:24 -0500

    First commit

---
+++ Output Blackout/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Output Blackout
+//
+//  Created by Omar Al-Ejel on 11/11/17.
+//  Copyright © 2017 Omar Al-Ejel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
